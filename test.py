67b069d19115b15de46a270572cb61d54ba4b1ed
Jaden Accord <jbr.accord@gmail.com>
2020-08-24 19:00:38 +0200

    Home scraping

--- test.py
+++ test.py
@@ -2,14 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-page = requests.get('https://www.hermannwesselinkcollege.nl/')
-soup = BeautifulSoup(page.content, 'html.parser')
-container = soup.find('div', class_='field-item even')
-children = container.findChildren(recursive=False)
+def formatHomePage(_title, _paragraphs):
+    body = '<html><body>'
+    body += str(_title)
+    for paragraph in _paragraphs:
+        if paragraph.text != "" and paragraph.text != " ":
+            body += '<p>{}<p>'.format(paragraph)
+    body += '</body></html>'
 
-for child in children:
-    if child.text != "":
-        print(child.text)
-        file = open('test.txt', 'a')
-        file.write(child.text + '\n')
-        file.close()
+    print(body)
+
+def main():
+    page = requests.get('https://www.hermannwesselinkcollege.nl/')
+    soup = BeautifulSoup(page.content, 'html.parser')
+    title = soup.find('h1', class_='title')
+    container = soup.find('div', class_='field-item even')
+    children = container.findChildren(recursive=False)
+    formatHomePage(title, children)
+
+if __name__ == '__main__':
+    main()
